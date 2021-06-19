e4fd331b737da0082fccf17ead5c56008d7bc6bf
Daniel Magueta <daniel.magueta@hotmail.com>
2021-06-19 11:06:09 +0100

    initial setup

---
+++ DiceRoller/DiceRoller.ip_user_files/sim_scripts/design_1/xcelium/run.f
@@ -0,0 +1,191 @@
+-makelib xcelium_lib/xpm -sv \
+  "C:/Xilinx/Vivado/2020.2/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "C:/Xilinx/Vivado/2020.2/data/ip/xpm/xpm_fifo/hdl/xpm_fifo.sv" \
+  "C:/Xilinx/Vivado/2020.2/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib xcelium_lib/xpm \
+  "C:/Xilinx/Vivado/2020.2/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib xcelium_lib/microblaze_v11_0_4 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/9285/hdl/microblaze_v11_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_microblaze_0_0/sim/design_1_microblaze_0_0.vhd" \
+-endlib
+-makelib xcelium_lib/lmb_v10_v3_0_11 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/c2ed/hdl/lmb_v10_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_dlmb_v10_0/sim/design_1_dlmb_v10_0.vhd" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_ilmb_v10_0/sim/design_1_ilmb_v10_0.vhd" \
+-endlib
+-makelib xcelium_lib/lmb_bram_if_cntlr_v4_0_19 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/0b98/hdl/lmb_bram_if_cntlr_v4_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_dlmb_bram_if_cntlr_0/sim/design_1_dlmb_bram_if_cntlr_0.vhd" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_ilmb_bram_if_cntlr_0/sim/design_1_ilmb_bram_if_cntlr_0.vhd" \
+-endlib
+-makelib xcelium_lib/blk_mem_gen_v8_4_4 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/2985/simulation/blk_mem_gen_v8_4.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_lmb_bram_0/sim/design_1_lmb_bram_0.v" \
+-endlib
+-makelib xcelium_lib/generic_baseblocks_v2_1_0 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/b752/hdl/generic_baseblocks_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/axi_infrastructure_v1_1_0 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/ec67/hdl/axi_infrastructure_v1_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/axi_register_slice_v2_1_22 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/af2c/hdl/axi_register_slice_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/fifo_generator_v13_2_5 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/276e/simulation/fifo_generator_vlog_beh.v" \
+-endlib
+-makelib xcelium_lib/fifo_generator_v13_2_5 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/276e/hdl/fifo_generator_v13_2_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/fifo_generator_v13_2_5 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/276e/hdl/fifo_generator_v13_2_rfs.v" \
+-endlib
+-makelib xcelium_lib/axi_data_fifo_v2_1_21 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/54c0/hdl/axi_data_fifo_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/axi_crossbar_v2_1_23 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/bc0a/hdl/axi_crossbar_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_xbar_0/sim/design_1_xbar_0.v" \
+-endlib
+-makelib xcelium_lib/axi_lite_ipif_v3_0_4 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/66ea/hdl/axi_lite_ipif_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/axi_intc_v4_1_15 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/47b8/hdl/axi_intc_v4_1_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_microblaze_0_axi_intc_0/sim/design_1_microblaze_0_axi_intc_0.vhd" \
+-endlib
+-makelib xcelium_lib/xlconcat_v2_1_4 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/4b67/hdl/xlconcat_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_microblaze_0_xlconcat_0/sim/design_1_microblaze_0_xlconcat_0.v" \
+-endlib
+-makelib xcelium_lib/mdm_v3_2_19 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/8715/hdl/mdm_v3_2_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_mdm_1_0/sim/design_1_mdm_1_0.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_clk_wiz_1_0/design_1_clk_wiz_1_0_clk_wiz.v" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_clk_wiz_1_0/design_1_clk_wiz_1_0.v" \
+-endlib
+-makelib xcelium_lib/lib_cdc_v1_0_2 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/ef1e/hdl/lib_cdc_v1_0_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/proc_sys_reset_v5_0_13 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/8842/hdl/proc_sys_reset_v5_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_rst_clk_wiz_1_100M_0/sim/design_1_rst_clk_wiz_1_100M_0.vhd" \
+-endlib
+-makelib xcelium_lib/interrupt_control_v3_1_4 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/a040/hdl/interrupt_control_v3_1_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/axi_gpio_v2_0_24 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/4318/hdl/axi_gpio_v2_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_axi_gpio_0_0/sim/design_1_axi_gpio_0_0.vhd" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_axi_gpio_1_0/sim/design_1_axi_gpio_1_0.vhd" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_axi_gpio_2_0/sim/design_1_axi_gpio_2_0.vhd" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_axi_gpio_3_0/sim/design_1_axi_gpio_3_0.vhd" \
+-endlib
+-makelib xcelium_lib/lib_pkg_v1_0_2 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/0513/hdl/lib_pkg_v1_0_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/lib_srl_fifo_v1_0_2 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/51ce/hdl/lib_srl_fifo_v1_0_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/axi_uartlite_v2_0_26 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/5edb/hdl/axi_uartlite_v2_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_axi_uartlite_0_0/sim/design_1_axi_uartlite_0_0.vhd" \
+-endlib
+-makelib xcelium_lib/axi_timer_v2_0_24 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/70d6/hdl/axi_timer_v2_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_axi_timer_0_0/sim/design_1_axi_timer_0_0.vhd" \
+-endlib
+-makelib xcelium_lib/fit_timer_v2_0_10 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/e81b/hdl/fit_timer_v2_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_fit_timer_0_0/sim/design_1_fit_timer_0_0.vhd" \
+-endlib
+-makelib xcelium_lib/emc_common_v3_0_5 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/ea80/hdl/emc_common_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/axi_emc_v3_0_22 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/38cd/hdl/axi_emc_v3_0_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_axi_emc_0_0/sim/design_1_axi_emc_0_0.vhd" \
+-endlib
+-makelib xcelium_lib/lib_fifo_v1_0_14 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/a5cb/hdl/lib_fifo_v1_0_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/axi_datamover_v5_1_24 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/4ab6/hdl/axi_datamover_v5_1_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/axi_sg_v4_1_13 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/4919/hdl/axi_sg_v4_1_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/axi_dma_v7_1_23 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/89d8/hdl/axi_dma_v7_1_vh_rfs.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_axi_dma_0_0/sim/design_1_axi_dma_0_0.vhd" \
+-endlib
+-makelib xcelium_lib/axis_infrastructure_v1_1_0 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/8713/hdl/axis_infrastructure_v1_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/axis_data_fifo_v2_0_4 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ipshared/abd4/hdl/axis_data_fifo_v2_0_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_axis_data_fifo_0_0/sim/design_1_axis_data_fifo_0_0.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../bd/design_1/ipshared/f2bb/hdl/DiceNumberGeneratorCop_v1_0.vhd" \
+  "../../../bd/design_1/ipshared/f2bb/hdl/DiceNumberGeneratorCop_v1_0_M00_AXIS.vhd" \
+  "../../../bd/design_1/ipshared/f2bb/hdl/DiceNumberGeneratorCop_v1_0_S00_AXIS.vhd" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_DiceNumberGeneratorC_0_0/sim/design_1_DiceNumberGeneratorC_0_0.vhd" \
+-endlib
+-makelib xcelium_lib/axi_protocol_converter_v2_1_22 \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.srcs/sources_1/bd/design_1/ipshared/5cee/hdl/axi_protocol_converter_v2_1_vl_rfs.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_auto_pc_8/sim/design_1_auto_pc_8.v" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_auto_pc_0/sim/design_1_auto_pc_0.v" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_auto_pc_1/sim/design_1_auto_pc_1.v" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_auto_pc_2/sim/design_1_auto_pc_2.v" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_auto_pc_3/sim/design_1_auto_pc_3.v" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_auto_pc_4/sim/design_1_auto_pc_4.v" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_auto_pc_5/sim/design_1_auto_pc_5.v" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_auto_pc_6/sim/design_1_auto_pc_6.v" \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/ip/design_1_auto_pc_7/sim/design_1_auto_pc_7.v" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../../../../OneDrive - Universidade de Aveiro/Joo Abrantes/UAveiro/6Ano/CR/DMA/project_DMA/project_DMA.gen/sources_1/bd/design_1/sim/design_1.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
